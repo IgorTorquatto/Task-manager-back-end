64e5361d1ae7b1686f54df61b31562ecbffdca57
Igor <igor.torquato.20@gmail.com>
2025-01-13 17:57:55 -0300

    add: encryption of user passwords bcrypt

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 const app = express();
 
 const port = 3000;
@@ -23,7 +24,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Rotas
-app.post("/create_user", (req, res) => {
+app.post("/create_user", async (req, res) => {
     const { name, email, password } = req.body;
   
     // Verificar se os campos obrigatórios estão presentes
@@ -31,23 +32,32 @@ app.post("/create_user", (req, res) => {
       return res.status(400).json({ error: "Todos os campos são obrigatórios" });
     }
   
-    // Inserir o usuário no banco
-    const query = `
-      INSERT INTO users (name, email, password, created_at, updated_at) 
-      VALUES (?, ?, ?, NOW(), NOW())
-    `;
-    
-    db.query(query, [name, email, password], (err, result) => {
-      if (err) {
-        console.error("Erro ao criar usuário:", err);
-        return res.status(500).json({ error: "Erro ao criar usuário" });
-      }
-  
-      res.status(201).json({
-        message: "Usuário criado com sucesso",
-        userId: result.insertId, // Retorna o ID do usuário inserido
+    try{
+        //Gerar hash de senha
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password,saltRounds);
+
+        // Query  SQL para inserir o usuário no banco
+        const query = `
+          INSERT INTO users (name, email, password, created_at, updated_at, deleted_at) 
+          VALUES (?, ?, ?, NOW(), NOW(), NULL)
+        `;
+        
+        db.query(query, [name, email, hashedPassword], (err, result) => {
+          if (err) {
+            console.error("Erro ao criar usuário:", err);
+            return res.status(500).json({ error: "Erro ao criar usuário" });
+          }
+      
+          res.status(201).json({
+            message: "Usuário criado com sucesso",
+            userId: result.insertId, // Retorna o ID do usuário inserido
+          });
       });
-    });
+    }catch(err){
+        console.log("Erro ao criptografar senha", err);
+        res.status(500).json({error: "Erro ao criar usuário"});
+    }
 });
 
 app.listen(port,()=>{
